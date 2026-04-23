b373a350b6cb1f07101def2ac3766b872a9123b6
ibrahim9071 <infinitypo35@gmail.com>
2026-04-23 11:04:24 +0300

    Update update.py

--- streams/update.py
+++ streams/update.py
@@ -2,28 +2,26 @@ import requests
 
 USERNAME = "kennyhoopz"
 
-def get_m3u8():
+def get_stream():
     try:
         url = f"https://kick.com/api/v2/channels/{USERNAME}"
         r = requests.get(url).json()
 
-        playback = r.get("playback_url")
-
-        return playback
+        if r.get("livestream"):
+            return r["livestream"]["playback_url"]
+        return None
     except:
         return None
 
-stream = get_m3u8()
+stream = get_stream()
 
 if stream:
     content = f"""#EXTM3U
-#EXTINF:-1,Kenny Hoopz (Kick)
+#EXTINF:-1,Kick Live
 {stream}
 """
 else:
-    content = """#EXTM3U
-#EXTINF:-1,OFFLINE
-"""
+    content = "#EXTM3U\n#EXTINF:-1,OFFLINE\n"
 
 with open("streams/live.m3u8", "w") as f:
     f.write(content)
